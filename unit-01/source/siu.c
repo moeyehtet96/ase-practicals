7920563e18b619c73773e7dab24610512d88835e
Moe Ye Htet <moeyehtet96@gmail.com>
2025-10-27 19:07:01 +0100

    unit-01: edit comments to include PCR number in pin configurations

--- unit-01/source/siu.c
+++ unit-01/source/siu.c
@@ -25,28 +25,28 @@ void SIU_Init(void)
 	/* Input pin configurations */
 
 	// SIU.PCR[...].R = ...;
-	SIU.PCR[60].R = 0x0100; // BT1 - D[12]
-	SIU.PCR[62].R = 0x0100; // BT2 - D[14]
-	SIU.PCR[52].R = 0x0100; // SW1 - D[4]
-	SIU.PCR[53].R = 0x0100; // SW2 - D[5]
-	SIU.PCR[54].R = 0x0100; // SW3 - D[6]
-	SIU.PCR[55].R = 0x0100; // SW4 - D[7]
+	SIU.PCR[60].R = 0x0100; // BT1 - D[12] - PCR[60]
+	SIU.PCR[62].R = 0x0100; // BT2 - D[14] - PCR[62]
+	SIU.PCR[52].R = 0x0100; // SW1 - D[4] - PCR[52]
+	SIU.PCR[53].R = 0x0100; // SW2 - D[5] - PCR[53]
+	SIU.PCR[54].R = 0x0100; // SW3 - D[6] - PCR[54]
+	SIU.PCR[55].R = 0x0100; // SW4 - D[7] - PCR[55]
 
 	/* Output pin configurations */
 
 	// SIU.PCR[...].R = ...;
-	SIU.PCR[56].R = 0x0200; // P - Red LED
-	SIU.PCR[57].R = 0x0200; // Rx - Yellow LED
-	SIU.PCR[58].R = 0x0200; // Tx - Yellow LED
-	SIU.PCR[59].R = 0x0200; // U1 - Green LED
-	SIU.PCR[43].R = 0x0200; // U2 - Green LED
-	SIU.PCR[6].R = 0x0200;	// U3 - Green LED
+	SIU.PCR[56].R = 0x0200; // P - Red LED - PCR[56]
+	SIU.PCR[57].R = 0x0200; // Rx - Yellow LED - PCR[57]
+	SIU.PCR[58].R = 0x0200; // Tx - Yellow LED - PCR[58]
+	SIU.PCR[59].R = 0x0200; // U1 - Green LED - PCR[59]
+	SIU.PCR[43].R = 0x0200; // U2 - Green LED - PCR[43]
+	SIU.PCR[6].R = 0x0200;	// U3 - Green LED - PCR[6]
 
 	/* Analog pin configurations */
 
 	// SIU.PCR[...].R = ...;
-	SIU.PCR[66].R = 0x2500; // POT
-	SIU.PCR[32].R = 0x2500; // LDR
+	SIU.PCR[66].R = 0x2500; // POT - PCR[66]
+	SIU.PCR[32].R = 0x2500; // LDR - PCR[32]
 
 	/*************************************************************************/
 }
